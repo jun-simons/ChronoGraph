74136f8551ba8f285f086cda511666e3f2a545a8
Jun Simons <jdsimons017@gmail.com>
2025-06-01 13:29:32 -0400

    create CMakeExtension class

--- setup.py
+++ setup.py
@@ -6,3 +6,13 @@ import platform
 import subprocess
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
+
+class CMakeExtension(Extension):
+    """
+    A setuptools.Extension for a CMake‐based build.  
+    `name` must match the module name exposed via PYBIND11_MODULE in c++ code
+    `sourcedir` points to the directory containing top-level CMakeLists.txt.
+    """
+    def __init__(self, name, sourcedir=""):
+        super().__init__(name, sources=[])
+        self.sourcedir = os.path.abspath(sourcedir)
